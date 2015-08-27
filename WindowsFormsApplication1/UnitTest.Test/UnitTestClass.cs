8a0145d3ef94ee6c56fb68bc43b0b34260a5ac94
Aparnaram <aparna.subramanian@verizon.com>
2015-08-27 00:39:10 -0700

    Added unit test class

--- WindowsFormsApplication1/UnitTest.Test/UnitTestClass.cs
+++ WindowsFormsApplication1/UnitTest.Test/UnitTestClass.cs
@@ -4,17 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-
+using WindowsFormsApplication1;
 namespace UnitTest.Test
 {
     [TestFixture]
     public class UnitTestClass
     {
-        //[TestCase]
-        //public int add(int a,int b)
-        //{
-            
-        //    Assert.AreEqual(50,)
-        //}
+        [TestCase]
+        public void add(int a, int b)
+        {
+            Arithmetic arithmetic = new Arithmetic();
+            int result = arithmetic.add(10, 40);
+            Assert.AreEqual(60, result);
+        }
     }
 }
